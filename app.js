a360153284854f587d34c18ef592cd92482f0829
heliopiovezan <heliopiovezan@yahoo.com.br>
2021-04-24 20:50:48 -0300

    implementação do cors tentativa2

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import { db } from './models/index.js';
 import { gradeRouter } from './routes/gradeRouter.js';
 import dotenv from 'dotenv';
 dotenv.config();
-cors();
 
 (async () => {
   try {
@@ -22,14 +21,14 @@ cors();
 })();
 
 const app = express();
-// app.use((req, res, next) => {
-//   //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-//   res.header('Access-Control-Allow-Origin', '*');
-//   //Quais são os métodos que a conexão pode realizar na API
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   app.use(cors());
-//   next();
-// });
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header('Access-Control-Allow-Origin', '*');
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 app.use(express.json());
 app.use(gradeRouter);
 
