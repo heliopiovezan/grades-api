d77a6113264a6a4b804a3f39f579ca6a34ab61fe
heliopiovezan <heliopiovezan@yahoo.com.br>
2021-04-24 21:00:09 -0300

    correção cors - uso do cors autorizado pelo modelo original

--- app.js
+++ app.js
@@ -21,23 +21,23 @@ dotenv.config();
 })();
 
 const app = express();
-app.use((req, res, next) => {
-  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-  res.header('Access-Control-Allow-Origin', '*');
-  //Quais são os métodos que a conexão pode realizar na API
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  app.use(cors());
-  next();
-});
 app.use(express.json());
 app.use(gradeRouter);
+// app.use((req, res, next) => {
+//   //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+//   res.header('Access-Control-Allow-Origin', '*');
+//   //Quais são os métodos que a conexão pode realizar na API
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   app.use(cors());
+//   next();
+// });
 
 //define o dominio de origem para consumo do servico
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: '*',
   })
 );
 
