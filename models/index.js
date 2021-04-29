1594d69d9345cbd2c1ad74eaf7d5a5b262af3bd9
heliopiovezan <heliopiovezan@yahoo.com.br>
2021-04-29 13:02:33 -0300

    Publicação original Hélio

--- models/index.js
+++ models/index.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { isLocal } from '../localOnly/localOnly.js';
 
 dotenv.config();
 
 const db = {};
 db.mongoose = mongoose;
 db.url = process.env.MONGODB;
-isLocal();
+
 export { db };
